5aa41383e21dd0a55998e518a76961be5bc82cf5
Tommy <tommy_8786@hotmail.com>
2020-09-30 11:22:29 +0200

    First commit

---
+++ Lab3/MetadataReader/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+namespace MetadataReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input filepath to be read: ");
+            //string input = Console.ReadLine();
+            
+            StreamReader streamreader = new StreamReader(@"C:\Users\Tommy\Desktop\banana.png");
+            Console.WriteLine(streamreader.ReadToEnd()); 
+        }
+    }
+}
